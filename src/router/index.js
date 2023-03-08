091c5dbfcb6960474ffb011c55a347de126cb548
Soulaimane <82826040+soulaimane0@users.noreply.github.com>
2023-03-08 22:44:40 +0100

    Update router

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,41 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import PageHome from '@/pages/PageHome.vue';
-import PageThreadShow from '@/pages/PageThreadShow.vue';
+import sourceData from '@/data.json';
+import Home from '@/pages/Home.vue';
+import ThreadShow from '@/pages/ThreadShow.vue';
+import NotFound from '@/pages/NotFound.vue';
 
 const routes = [
   {
+    path: '/threads',
+    redirect: '/',
+  },
+  {
     path: '/',
-    component: PageHome,
-    name: 'Home',
+    component: Home,
+    name: 'home',
   },
   {
     path: '/thread/:id',
-    component: PageThreadShow,
+    component: ThreadShow,
     name: 'thread',
+    beforeEnter(to, from, next) {
+      //chack is the thread id exists
+      const thread = sourceData.threads.find((thread) => thread.id === to.params.id);
+      if (thread) return next();
+      else {
+        next({
+          name: 'notFound',
+          params: { pathMatch: to.path.substring(1).split('/') },
+          query: to.query,
+          hash: to.hash,
+        });
+      }
+    },
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    component: NotFound,
+    name: 'notFound',
   },
 ];
 
