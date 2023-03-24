cb1f929eddc477651a0b7db66aaa304bd00c664d
Soulaimane <82826040+soulaimane0@users.noreply.github.com>
2023-03-24 13:54:19 +0100

    decorate code

--- src/pages/ThreadShow.vue
+++ src/pages/ThreadShow.vue
@@ -3,13 +3,17 @@ import { computed, reactive, ref } from 'vue';
 import { useRoute } from 'vue-router';
 import useThreadStore from '@/stores/ThreadStore';
 import usePostsStore from '@/stores/PostsStore';
-import { storeToRefs } from 'pinia';
 
-const { thread } = storeToRefs(useThreadStore());
+const props = defineProps(['forumId']);
+const route = useRoute();
+const threadStore = useThreadStore();
 const postsStore = usePostsStore();
 
-const route = useRoute();
 const threadId = route.params.id;
+const thread = threadStore.thread(threadId);
+const user = threadStore.getUserByThread(thread.userId);
+const total_posts = threadStore.countThreadPosts(threadId);
+const total_contributors = threadStore.countThreadContributors(threadId);
 
 const savePost = (text) => {
   postsStore.save(text, threadId);
@@ -18,10 +22,27 @@ const savePost = (text) => {
 
 <template>
   <div class="d-flex justify-content-between">
-    <h1 class="mb-3">{{ thread(threadId).title }}</h1>
+    <h1 class="mb-3">{{ thread.title }}</h1>
     <div class="d-grid align-items-center">
-      <button class="btn btn-primary fw-semibold">Update thread</button>
+      <RouterLink :to="{ name: 'thread-update', params: { id: threadId } }">
+        <button class="btn btn-primary fw-semibold">Update thread</button>
+      </RouterLink>
     </div>
+  </div>
+  <div class="d-flex justify-content-between text-secondary">
+    <p>
+      By {{ user.name }},
+      <BaseDate :timestamp="thread.publishedAt" />
+    </p>
+    <p>
+      {{
+        `${total_posts} ${
+          total_posts > 1 ? 'Replies' : 'Reply'
+        } by ${total_contributors} ${
+          total_contributors > 1 ? 'Contributors' : 'Contributor'
+        }`
+      }}
+    </p>
   </div>
 
   <PostEditor @save-post="savePost" />
