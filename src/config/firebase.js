88b22bfd86bdb46286f1a16e7cd76c4547daf9cc
Soulaimane <82826040+soulaimane0@users.noreply.github.com>
2023-03-29 16:24:48 +0200

    initialize and add firebase config

---
+++ src/config/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAy93_bgMy5QUHF_DTBiodaSZRGG4A1mJ4',
+  authDomain: 'forum-26ac1.firebaseapp.com',
+  projectId: 'forum-26ac1',
+  storageBucket: 'forum-26ac1.appspot.com',
+  messagingSenderId: '31040409418',
+  appId: '1:31040409418:web:99c97ffbaa161e99533bb3',
+};
+
+export default firebaseConfig;
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from '@/router';
+import { initializeApp } from 'firebase/app';
+import firebaseConfig from './config/firebase.js';
+('@/config/firebase.js');
 import { createPinia } from 'pinia';
 
 //Bootstrap links
@@ -8,6 +11,8 @@ import 'bootstrap/dist/css/bootstrap.css';
 import BootstrapPlugin from '@/plugins/bootstrap.js';
 
 import '@/sass/main.scss';
+
+const app = initializeApp(firebaseConfig);
 
 const forumApp = createApp(App);
 const pinia = createPinia();
