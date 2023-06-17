905ce79462e48b0bdac87093d0a26bf6f56c1679
Soulaimane <82826040+soulaimane0@users.noreply.github.com>
2023-06-17 22:54:24 +0100

    Use fontawesome plugin

--- src/main.js
+++ src/main.js
@@ -3,11 +3,15 @@ import App from './App.vue';
 import router from '@/router';
 import { createPinia } from 'pinia';
 
-//Bootstrap links
+// Bootstrap links
 import 'bootstrap/dist/css/bootstrap.css';
 import BootstrapPlugin from '@/plugins/bootstrap.js';
 
+// Sass file
 import '@/sass/main.scss';
+
+// Font Awesome Plugin
+import FontAwesome from '@/plugins/FontAwesome';
 
 const forumApp = createApp(App);
 const pinia = createPinia();
@@ -15,4 +19,5 @@ const pinia = createPinia();
 forumApp.use(router);
 forumApp.use(pinia);
 forumApp.use(BootstrapPlugin);
+forumApp.use(FontAwesome);
 forumApp.mount('#app');
