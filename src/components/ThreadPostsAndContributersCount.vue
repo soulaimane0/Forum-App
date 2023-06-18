da148faa06c576714057df534842e0a55844debb
Soulaimane <82826040+soulaimane0@users.noreply.github.com>
2023-06-18 22:07:58 +0100

    Clean the PACC component and update code

--- src/components/ThreadPostsAndContributersCount.vue
+++ src/components/ThreadPostsAndContributersCount.vue
@@ -1,29 +1,22 @@
 <script setup>
-import { onMounted, ref } from 'vue';
-import useThreadStore from '@/stores/ThreadStore';
-
 const props = defineProps({
-  threadId: {
-    type: String,
+  postsNum: {
+    type: Number,
     required: true,
   },
-});
-const threadStore = useThreadStore();
-const total_posts = ref(null);
-const total_contributors = ref(null);
-
-onMounted(async () => {
-  total_posts.value = await threadStore.countThreadPosts(props.threadId);
-  total_contributors.value = await threadStore.countThreadContributors(props.threadId);
+  contibutorsNum: {
+    type: Number,
+    required: true,
+  },
 });
 </script>
 
 <template>
   <p>
     {{
-      `${total_posts ? total_posts : '..'} ${total_posts > 1 ? 'Replies' : 'Reply'} by ${
-        total_contributors ? total_contributors : '..'
-      } ${total_contributors > 1 ? 'Contributors' : 'Contributor'}`
+      `${postsNum ? postsNum : '..'} ${postsNum > 1 ? 'Replies' : 'Reply'} by ${
+        contibutorsNum ? contibutorsNum : '..'
+      } ${contibutorsNum > 1 ? 'Contributors' : 'Contributor'}`
     }}
   </p>
 </template>
