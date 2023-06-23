2dd23067298fbf37ef5b1efce5b9348183c9173e
Soulaimane <82826040+soulaimane0@users.noreply.github.com>
2023-06-23 21:26:25 +0100

    add new icons

--- src/plugins/FontAwesome.js
+++ src/plugins/FontAwesome.js
@@ -3,10 +3,18 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 // Import Font Awesome Component
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 // Import specific icons
-import { faPencil } from '@fortawesome/free-solid-svg-icons';
+import {
+  faPencil,
+  faUser,
+  faUsers,
+  faComments,
+  faComment,
+} from '@fortawesome/free-solid-svg-icons';
+import { faGoogle } from '@fortawesome/free-brands-svg-icons';
+// import { faComments, faComment } from '@fortawesome/free-regular-svg-icons';
 // Add Icon to the library
 
-library.add(faPencil);
+library.add(faPencil, faGoogle, faUser, faUsers, faComments, faComment);
 
 export default (app) => {
   app.component('fa', FontAwesomeIcon);
