70823343f29a1f0bd78108a5af14528815c5416e
Soulaimane <82826040+soulaimane0@users.noreply.github.com>
2023-06-23 21:25:44 +0100

    export firebase authentication

--- src/helpers/firestore.js
+++ src/helpers/firestore.js
@@ -1,8 +1,10 @@
 import { initializeApp } from 'firebase/app';
 import firebaseConfig from '@/config/firebaseConfig.js';
 import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
 
 // export default { db };
