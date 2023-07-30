d0a39fc0d9e302bdb4da142bbfb8857b2e5691f0
Soulaimane <boukrabsolayman402@gmail.com>
2023-07-31 00:03:54 +0100

    Create array random helper

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -5,3 +5,7 @@ export const findById = (resources, id) => resources.find((item) => item.id === 
 export const handleImgError = (e) => (e.target.src = '/user-placeholder.png');
 export const appendUnsubscribe = (unsubscribe) =>
   useUnsubscribeStore().unsubscribes.push(unsubscribe);
+export const arrayRandom = (array) => {
+  const randomIndex = Math.floor(Math.random() * array.length);
+  return array[randomIndex];
+};
